175cfbaa24da117b29c3d4c787306972988e3ce8
DESKTOP-7AU1H3Q\Yunus <ostbat43@gmail.com>
2021-11-16 02:06:24 +0500

    fomated

--- app/Services/Localization/Localization.php
+++ app/Services/Localization/Localization.php
@@ -1,25 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: note
- * Date: 28.11.2020
- * Time: 8:19
- */
 
 namespace App\Services\Localization;
 
-
 use Illuminate\Support\Facades\App;
 
-class Localization
-{
+class Localization {
+
     public function locale() {
         $locale = request()->segment(1);
-        if($locale && in_array($locale, config('app.locales'))) {
+        if ($locale && in_array($locale, config('app.locales'))) {
             App::setLocale($locale);
             return $locale;
         }
 
         return '';
     }
+
 }
--- app/Services/Localization/LocalizationService.php
+++ app/Services/Localization/LocalizationService.php
@@ -1,20 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: note
- * Date: 28.11.2020
- * Time: 8:16
- */
 
 namespace App\Services\Localization;
 
-
 use Illuminate\Support\Facades\Facade;
 
-class LocalizationService extends Facade
-{
-    protected static  function getFacadeAccessor()
-    {
+class LocalizationService extends Facade {
+
+    protected static function getFacadeAccessor() {
         return 'Localization';
     }
+
 }
